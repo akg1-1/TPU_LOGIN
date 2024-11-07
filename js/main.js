0d3ded55542a5a8b71817a573e84a25d04eb81ce
Andre <akg7@tpu.ru>
2024-11-07 18:17:28 +0700

    Final laptop version

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
     
     /*==================================================================
     [ Validate ]*/
-    var input = $('.validate-input .input100');
+    var input = $('.validate-input .input-form');
 
     $('.validate-form').on('submit',function(){
         var check = true;
@@ -21,7 +21,7 @@
     });
 
 
-    $('.validate-form .input100').each(function(){
+    $('.validate-form .input-form').each(function(){
         $(this).focus(function(){
            hideValidate(this);
         });
